40a9ddd18563110305d5b6083f26fe03b3fba453
argjiramala <52406288+argjiramala@users.noreply.github.com>
2021-03-23 21:20:32 +0100

    Project setup

---
+++ Product-Reviews/Product-Reviews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Product-Reviews
+//
+//  Created by Argjira Mala on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Product-Reviews/Product-ReviewsTests/Product_ReviewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Product_ReviewsTests.swift
+//  Product-ReviewsTests
+//
+//  Created by Argjira Mala on 3/23/21.
+//
+
+import XCTest
+@testable import Product_Reviews
+
+class Product_ReviewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
